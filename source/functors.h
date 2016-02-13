7494c0e619094b8a4b7ecae357df976aac13c382
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-02-13 17:16:27 +0100

    Fix issue with functors and temporary objects

--- source/functors.h
+++ source/functors.h
@@ -4,46 +4,46 @@
 /*math operators functors*/
 
 const auto add = [](const auto& x) {
-	return [&](const auto& n) { return n + x; };
+	return [=](const auto& n) { return n + x; };
 };
 
 const auto subtract = [](const auto& x) {
-	return [&](const auto& n) { return n - x; };
+	return [=](const auto& n) { return n - x; };
 };
 
 const auto multiplyBy = [](const auto& x) {
-	return [&](const auto& n) { return n * x; };
+	return [=](const auto& n) { return n * x; };
 };
 
 const auto divideBy = [](const auto& x) {
 	if (x == 0) throw std::logic_error("divide by zero");
-	return [&](const auto& n) { return n / x; };
+	return [=](const auto& n) { return n / x; };
 };
 
 /*boolean operators functors*/
 
 const auto lessThan = [](const auto& x) {
-	return [&](const auto& n) { return n < x; };
+	return [=](const auto& n) { return n < x; };
 };
 
 const auto greaterThan = [](const auto& x) {
-	return [&](const auto& n) { return n > x; };
+	return [=](const auto& n) { return n > x; };
 };
 
 const auto lessOrEqual = [](const auto& x) {
-	return [&](const auto& n) { return n <= x; };
+	return [=](const auto& n) { return n <= x; };
 };
 
 const auto greaterOrEqual = [](const auto& x) {
-	return [&](const auto& n) { return n >= x; };
+	return [=](const auto& n) { return n >= x; };
 };
 
 const auto equalTo = [](const auto& x) {
-	return [&](const auto& n) { return n == x; };
+	return [=](const auto& n) { return n == x; };
 };
 
 const auto notEqualTo = [](const auto& x) {
-	return [&](const auto& n) { return x != n; };
+	return [=](const auto& n) { return x != n; };
 };
 
 

34f3051b1c2b815891d7e964917ae766563ae43e
Jonatan Kłosko <jonatanklosko@gmail.com>
2016-02-18 15:38:44 +0100

    Add test cases for the boolean functors

--- test/functors_test.cpp
+++ test/functors_test.cpp
@@ -71,3 +71,120 @@ TEST_CASE("divideBy", "[functors][math]") {
 		REQUIRE_THROWS( divideBy(0)(1.1) );
 	}
 }
+
+TEST_CASE("lessThan", "[functors][boolean]") {
+
+	SECTION("should return function that returns if its own argument is less than the given argument") {
+		auto lessThanFive = lessThan(5);
+		REQUIRE( lessThanFive(3) );
+		REQUIRE_FALSE( lessThanFive(7.7) );
+
+		REQUIRE( lessThan(15)(10.5) );
+		REQUIRE_FALSE( lessThan(5)(5) );
+		REQUIRE( lessThan(1)(-10) );
+		REQUIRE_FALSE( lessThan(-1)(0) );
+	}
+}
+
+TEST_CASE("greaterThan", "[functors][boolean]") {
+
+	SECTION("should return function that returns if its own argument is greater than the given argument") {
+		auto greaterThanFive = greaterThan(5);
+		REQUIRE( greaterThanFive(7) );
+		REQUIRE_FALSE( greaterThanFive(3.3) );
+
+		REQUIRE( greaterThan(10)(15) );
+		REQUIRE_FALSE( greaterThan(5)(5) );
+		REQUIRE( greaterThan(-10)(-1) );
+		REQUIRE_FALSE( greaterThan(0)(-1.1) );
+	}
+}
+
+TEST_CASE("lessOrEqual", "[functors][boolean]") {
+
+	SECTION("should return function that returns if its own argument is less than or equal to the given argument") {
+		auto lessOrEqualFive = lessOrEqual(5);
+		REQUIRE( lessOrEqualFive(3) );
+		REQUIRE_FALSE( lessOrEqualFive(7.7) );
+
+		REQUIRE( lessOrEqual(15)(10.5) );
+		REQUIRE( lessOrEqual(5)(5) );
+		REQUIRE_FALSE( lessOrEqual(5)(5.1) );
+		REQUIRE( lessOrEqual(-10)(-10.0) );
+		REQUIRE( lessOrEqual(1)(-10) );
+		REQUIRE_FALSE( lessOrEqual(-1)(0) );
+	}
+}
+
+TEST_CASE("greaterOrEqual", "[functors][boolean]") {
+
+	SECTION("should return function that returns if its own argument is greater than or equal to the given argument") {
+		auto greaterOrEqualFive = greaterOrEqual(5);
+		REQUIRE( greaterOrEqualFive(7) );
+		REQUIRE_FALSE( greaterOrEqualFive(3.3) );
+
+		REQUIRE( greaterOrEqual(10)(15) );
+		REQUIRE( greaterOrEqual(5.0)(5) );
+		REQUIRE_FALSE( greaterOrEqual(5.1)(5) );
+		REQUIRE( greaterOrEqual(-10)(-1) );
+		REQUIRE_FALSE( greaterOrEqual(0)(-1.1) );
+	}
+}
+
+TEST_CASE("equalTo", "[functors][boolean]") {
+
+	SECTION("should return function that returns if its own argument is equal to the given argument") {
+		auto equalToFive = equalTo(5);
+		REQUIRE( equalToFive(5.0) );
+		REQUIRE_FALSE( equalToFive(7.7) );
+
+		REQUIRE( equalTo(15)(15) );
+		REQUIRE_FALSE( equalTo(5)(-5) );
+		REQUIRE_FALSE( equalTo(5)(5.1) );
+		REQUIRE( equalTo(-10)(-10.0) );
+	}
+}
+
+TEST_CASE("notEqualTo", "[functors][boolean]") {
+
+	SECTION("should return function that returns if its own argument is not equal to the given argument") {
+		auto notEqualToFive = notEqualTo(5);
+		REQUIRE( notEqualToFive(7.7) );
+		REQUIRE_FALSE( notEqualToFive(5) );
+
+		REQUIRE( notEqualTo(15)(-15) );
+		REQUIRE_FALSE( notEqualTo(5)(5) );
+		REQUIRE_FALSE( notEqualTo(5)(5.0) );
+		REQUIRE( notEqualTo(-10)(-10.12) );
+	}
+}
+
+TEST_CASE("odd", "[functions][math]") {
+
+	SECTION("should return true if the given argument is odd") {
+		REQUIRE( odd(1) );
+		REQUIRE( odd(11) );
+		REQUIRE( odd(13432423431) );
+	}
+
+	SECTION("should return false if the given argument is even") {
+		REQUIRE_FALSE( odd(2) );
+		REQUIRE_FALSE( odd(104) );
+		REQUIRE_FALSE( odd(13432423438) );
+	}
+}
+
+TEST_CASE("even", "[functions][math]") {
+
+	SECTION("should return true if the given argument is even") {
+		REQUIRE( even(2) );
+		REQUIRE( even(104) );
+		REQUIRE( even(13432423438) );
+	}
+
+	SECTION("should return false if the given argument is odd") {
+		REQUIRE_FALSE( even(1) );
+		REQUIRE_FALSE( even(11) );
+		REQUIRE_FALSE( even(13432423431) );
+	}
+}
